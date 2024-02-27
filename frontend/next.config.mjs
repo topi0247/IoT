90434a0a6cd2a20ec8b6a61d8fac66b6a3902abb
Kanae-Nishina <karubona.ra247@gmail.com>
2024-02-28 01:24:39 +0900

    Sentry導入（テストはまだ）

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,15 +1,63 @@
-import {withSentryConfig} from '@sentry/nextjs';
+import { withSentryConfig } from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default withSentryConfig(nextConfig, {
+export default withSentryConfig(withSentryConfig(withSentryConfig(nextConfig, {
+silent: false,
+org: "topi-3j",
+project: "iot-frontend",
+}, {
+widenClientFileUpload: true,
+
+transpileClientSDK: true,
+
+tunnelRoute: "/monitoring",
+
+hideSourceMaps: true,
+
+disableLogger: true,
+
+automaticVercelMonitors: true,
+}), {
 // For all available options, see:
 // https://github.com/getsentry/sentry-webpack-plugin#options
 
 // Suppresses source map uploading logs during build
 silent: true,
 org: "topi-3j",
-project: "iot-frontend",
+project: "imaginarium-of-the-table--frontend",
+}, {
+// For all available options, see:
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+// Upload a larger set of source maps for prettier stack traces (increases build time)
+widenClientFileUpload: true,
+
+// Transpiles SDK to be compatible with IE11 (increases bundle size)
+transpileClientSDK: true,
+
+// Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+tunnelRoute: "/monitoring",
+
+// Hides source maps from generated client bundles
+hideSourceMaps: true,
+
+// Automatically tree-shake Sentry logger statements to reduce bundle size
+disableLogger: true,
+
+// Enables automatic instrumentation of Vercel Cron Monitors.
+// See the following for more information:
+// https://docs.sentry.io/product/crons/
+// https://vercel.com/docs/cron-jobs
+automaticVercelMonitors: true,
+}), {
+// For all available options, see:
+// https://github.com/getsentry/sentry-webpack-plugin#options
+
+// Suppresses source map uploading logs during build
+silent: true,
+org: "topi-3j",
+project: "imaginarium-of-the-table--frontend",
 }, {
 // For all available options, see:
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
